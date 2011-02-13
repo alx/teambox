94586ce0f773ad7f8bff545c056bb7f3e090bf9b
Alexandre Girard <alx.girard@gmail.com>
2011-02-13 15:02:31 +0100

    add more sendmail options in application.rb

--- config/application.rb
+++ config/application.rb
@@ -91,7 +91,8 @@ module Teambox
           action_mailer.smtp_settings = smtp_settings
         when :sendmail
           action_mailer.delivery_method = :sendmail
-          action_mailer.smtp_settings = smtp_settings
+          action_mailer.perform_deliveries = true
+          action_mailer.raise_delivery_errors = true
         end
       end
 
